d742070570eb4e4978c0e3233c2605292a1f6e84
laisrmacedo <laisrodriguesmacedo@gmail.com>
2024-02-22 19:49:35 +0100

    refactor: feedbacks carrousel

--- script.js
+++ script.js
@@ -1,73 +1,129 @@
-let feedbacksClass = document.querySelector('.feedbacks')
-// console.log(feedbacks)
-let feedbacks = [
+const feedbacksDiv = document.querySelector('.feedback')
+const containerPoint = document.querySelector('#container-point')
+let currentIndex = 0;
+let intervalId;
+
+const feedbacks = [
   {
-    id: 2,
+    name: "Laiane Rodrigues",
+    info: "Cliente",
+    link: "",
+    text: "Minha experiência com o convite personalizado foi maravilhosa, tendo em vista que a maioria dos meus convidados eram de cidades diferentes da minha. Esse tipo de convite facilitou a distribuição e me ofereceu bastante praticidade para a organização do casamento. Além disso, todo o desing foi exatamente como sempre sonhei e Laís,  sempre muito atenciosa, deixou todos os detalhes  perfeitos. Amei demais e super recomendo a experiência."
+  },
+  {
+    name: "Laiane Rodrigues",
+    info: "Cliente",
+    link: "",
+    text: "Laís, eu amei o convite! Atendeu as minhas expectativas, ficou bem funcional, prático e muito lindo 😍. Atendimento excelente, obrigada pela paciência <3"
+  },
+  {
+    name: "Bruna França",
+    info: "Cliente",
+    link: "",
+    text: "Laís, eu amei o convite! Atendeu as minhas expectativas, ficou bem funcional, prático e muito lindo 😍. Atendimento excelente, obrigada pela paciência <3"
+  },
+  {
+    name: "Erivonaldo Silva",
+    info: "Cliente",
+    link: "",
+    text: "Ficou excelente, bonito e prático, gostei muito do controle de convidados e envio das senhas. Parabéns pelo excelente trabalho Laís."
+  },
+  {
     name: "Alexandre Machado",
     info: "Comentou pelo LinkedIn",
     link: "https://www.linkedin.com/feed/update/urn:li:activity:7110318668542001152/",
-    text: "Ficou incrível! Parabéns!!!"
+    text: "Ficou incrível! Parabéns!!! Como sempre vc se destaca tbm pelo ótimo design 👏👏👏"
   },
   {
-    id: 3,
     name: "Marcos Hamilton Santos",
     info: "Comentou pelo LinkedIn",
     link: "https://www.linkedin.com/feed/update/urn:li:activity:7110318668542001152/",
     text: "Parabéns, muito legal como sempre!"
   },
   {
-    id: 4,
     name: "Claudia Uhr",
     info: "Comentou pelo LinkedIn",
     link: "https://www.linkedin.com/feed/update/urn:li:activity:7110318668542001152/",
     text: "Que trabalho lindo Lais Rodrigues"
   },
   {
-    id: 5,
-    name: "Mariana Negrão Trancoso",
+    name: "Flávia Santos",
     info: "Comentou pelo LinkedIn",
     link: "https://www.linkedin.com/feed/update/urn:li:activity:7110318668542001152/",
-    text: "Que lindooo!!!!!"
+    text: "Ficou incrível!!! Parabéns Lais Rodrigues !!!"
   },
   {
-    id: 6,
     name: "Willian Menezes",
     info: "Comentou pelo LinkedIn",
     link: "https://www.linkedin.com/feed/update/urn:li:activity:7110318668542001152/",
     text: "parabeens Lais, gostei dms"
   },
   {
-    id: 7,
-    name: "Carlos Bergson Louzada",
+    name: "Pedro Maia Filho",
     info: "Comentou pelo LinkedIn",
     link: "https://www.linkedin.com/feed/update/urn:li:activity:7110318668542001152/",
-    text: "Uau, ficou sensacional haha <3"
-  },
-  {
-    id: 1,
-    name: "Laiane Rodrigues",
-    info: "Cliente",
-    link: "",
-    text: "Minha experiência com o convite personalizado foi maravilhosa, tendo em vista que a maioria dos meus convidados eram de cidades diferentes da minha. Esse tipo de convite facilitou a distribuição e me ofereceu bastante praticidade para a organização do casamento. Além disso, todo o desing foi exatamente como sempre sonhei e Laís,  sempre muito atenciosa, deixou todos os detalhes  perfeitos. Amei demais e super recomendo a experiência."
+    text: "Caraca, ficou muito bacana! Parabéns Laís"
   }
 ]
 
-const showFeedback = () => {
-  feedbacks.map((item) => {
-    feedbacksClass.innerHTML = `
-      <div>
-        <img/>
-        <span>
-        <h3>${item.name}</h3>
-        <a href="${item.link}" target="_blank"><h4>${item.info}</h4></a>
-        </span>
-      </div>
-      <p>${item.text}</p>
-    `
-  })
+const updateFeedback = () => {
+  feedbacksDiv.innerHTML = `
+    <div>
+      <img/>
+      <span>
+      <h3>${feedbacks[currentIndex].name}</h3>
+      <a href="${feedbacks[currentIndex].link}" target="_blank"><h4>${feedbacks[currentIndex].info}</h4></a>
+      </span>
+    </div>
+    <p>${feedbacks[currentIndex].text}</p>
+  `
+
+  containerPoint.innerHTML = ''
+  for (const i in feedbacks) {
+    if (i == currentIndex) {
+      containerPoint.innerHTML += `<div><span class="currentPoint"></span></div>`
+    } else {
+      containerPoint.innerHTML += `<div><span class="othersPoints"></span></div>`
+    }
+  }
+
+  startInterval();
 }
 
-showFeedback()
+updateFeedback()
+
+function nextSlide() {
+  currentIndex = (currentIndex + 1) % feedbacks.length;
+  updateFeedback();
+}
+
+function startInterval() {
+  clearInterval(intervalId);
+  intervalId = setInterval(nextSlide, 8000);
+}
+
+startInterval();
+
+let touchStartX = 0;
+let touchEndX = 0;
+let currentPosition = 0;
+
+feedbacksDiv.addEventListener('touchstart', (event) => {
+  touchStartX = event.touches[0].clientX;
+});
+feedbacksDiv.addEventListener('touchmove', (event) => {
+  touchEndX = event.touches[0].clientX;
+});
+feedbacksDiv.addEventListener('touchend', () => {
+  if (touchStartX < touchEndX && touchEndX - touchStartX > 50) {
+    currentIndex = (currentIndex - 1 + feedbacks.length) % feedbacks.length;
+  } else if (touchStartX > touchEndX && touchStartX - touchEndX > 50) {
+    currentIndex = (currentIndex + 1) % feedbacks.length;
+  }
+  updateFeedback()
+  touchStartX = 0;
+  touchEndX = 0;
+});
 
 // Localize o contêiner HTML
 var animationContainer = document.getElementById('lottie-animation');
